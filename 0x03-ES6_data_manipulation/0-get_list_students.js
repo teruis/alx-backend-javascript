869ce6ea8d1eb0894ff39b222e00ed7acf606c7a
teruis <126614240+teruis@users.noreply.github.com>
2024-04-15 23:17:59 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,14 +3,22 @@
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
-function getListStudents() {
-  const students = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 5, firstName: "Serena", location: "San Francisco" },
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
   ];
-  return students;
 }
-
-module.exports = getListStudents;
-
